6271025449c2bf3a053fd852074c2af218473fa3
sdacruz1 <stephanie_da_cruz@brown.edu>
2023-01-05 20:07:00 -0500

    added a basic search bar

--- src/views/partners/Partners.jsx
+++ src/views/partners/Partners.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import React, {useState} from 'react'
+import './Partners.css';
 
 export default function Partners() {
+  const searchBar = () => {}
+  const [searchInput, setSearchInput] = useState("")
+  const [searchResponse, setSearchResponse] = useState("")
+  
+  const handleChange = (e) => {
+    e.preventDefault();
+    setSearchInput(e.target.value);
+    if (searchInput.length > 0) {
+      setSearchResponse("searching...");
+    } else {
+      setSearchResponse("not searching");
+    }
+  };
+  
   return (
-    <div>Partners</div>
-  )
-}
+    <div>
+    <div class="partners-search-wrapper">
+      <input
+        class="partners-search"
+        type="text"
+        placeholder="Search"
+        onChange={handleChange}
+        value={searchInput} />
+    </div>
+    
+    <p>{searchResponse}</p>
+    
+    </div>
+    
+    )
+  }
+  
