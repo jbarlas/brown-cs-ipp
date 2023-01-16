65cb01aa297587ac0b253d0248353b85865d0cce
william-czh <william_c986@outlook.com>
2023-01-17 00:54:37 +0800

    feat:major updates to application page, mainly on layout and style

--- src/views/application/Application.jsx
+++ src/views/application/Application.jsx
@@ -2,19 +2,30 @@ import React from 'react'
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
 import Divider from '@mui/material/Divider';
-import { maxWidth } from '@mui/system';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormGroup from '@mui/material/FormGroup';
+import FormLabel from '@mui/material/FormLabel';
+import Checkbox from '@mui/material/Checkbox';
+import CardMedia from '@mui/material/CardMedia';
+import Switch from '@mui/material/Switch';
+import "./Application.css";
+
 
 export default function Application() {
   return (
     <div>
-<div id='code-block' align="center">
-      <br></br>
+    <div class='code-block' align="center" >
+    <br></br>
+    <br></br>
+    <br></br>
 <Grid
 justifyContent="center"
 alignItems="center">
@@ -26,18 +37,26 @@ alignItems="center">
         <Box
     component="form"
     sx={{
-      '& > :not(style)': { m: 1, width: '25ch' },
+      '& > :not(style)': { m: 2, width: '30ch' },
     }}
     noValidate
     autoComplete="off"
   >
-    <TextField id="outlined-basic" label="Appplication Code" variant="outlined" />
+    <TextField id="outlined-basic" label="Appplication Code" variant="outlined" color="secondary"/>
+    
   </Box>
+  <Button variant="contained" color="success">
+  Validate
+</Button>
   <Typography variant="body2">
+  <br></br>
     Don’t have a code yet? Please reach out to our partner relations staff at: brown-cs-ipp@brown.edu to be provided with one. In the mean time, feel free to browse our application below.
         </Typography>
       </CardContent>
     </Card>
+    <br></br>
+    <br></br>
+    <br></br>
 </Grid>
     </div>
     <br></br>
@@ -51,7 +70,7 @@ alignItems="center">
     <Box
       component="form"
       sx={{
-        '& .MuiTextField-root': { m: 1, width: '25ch' },
+        '& .MuiTextField-root': { m: 2, width: '25ch' },
       }}
       noValidate
       autoComplete="off"
@@ -60,72 +79,149 @@ alignItems="center">
         <TextField
           id="outlined-multiline-flexible"
           label="Company Name"
+          color="secondary"
           Company Name
-          maxRows={4}
         />
         <TextField
           id="outlined-textarea"
           label="Industry"
-          placeholder="Placeholder"
+          color="secondary"
           Industry
-        />
-        <TextField
-          id="outlined-multiline-static"
-          label="Website"
-          Website
-          rows={4}
         />
       </div>
       <div>
+      <TextField
+          id="standard-basic"
+          label="Website"
+          color="secondary"
+          Website
+        />
         <TextField
           id="filled-multiline-flexible"
           label="Size (Employees)"
+          color="secondary"
           Employees
-          maxRows={4}
-          variant="filled"
         />
-        <TextField
+      </div>
+      
+      <div class="formcontrol">
+       <TextField
           id="filled-textarea"
-          label="Multiline Placeholder"
-          placeholder="Placeholder"
-          multiline
-          variant="filled"
+          label="Office Locations"
+          placeholder="Providence"
+          color="secondary"
+          Office Locations
         />
-        <TextField
-          id="filled-multiline-static"
-          label="Multiline"
-          multiline
-          rows={4}
-          defaultValue="Default Value"
-          variant="filled"
-        />
+      {"\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0"}
+      <FormControl>
+      <FormLabel id="demo-radio-buttons-group-label" color="secondary">Business Type</FormLabel>
+      <RadioGroup
+        name="radio-buttons-group"
+        
+      >
+        <FormControlLabel value="For Profit" control={<Radio color="secondary"/>} label="For Profit"  />
+        <FormControlLabel value="Not For Profit" control={<Radio color="secondary"/>} label="Not For Profit" />
+        <FormControlLabel value="Non-Profit" control={<Radio color="secondary"/>} label="Non-Profit" />
+      </RadioGroup>
+    </FormControl>
+    {"\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0"}
+    
+      </div>
+      </Box>
+      <div>
+      <FormControlLabel control={<Checkbox defaultChecked color="secondary" />} label="Remote Accessibility?" />
       </div>
       <div>
-        <TextField
-          id="standard-multiline-flexible"
-          label="Multiline"
-          multiline
-          maxRows={4}
-          variant="standard"
-        />
-        <TextField
-          id="standard-textarea"
-          label="Multiline Placeholder"
-          placeholder="Placeholder"
-          multiline
-          variant="standard"
-        />
-        <TextField
-          id="standard-multiline-static"
-          label="Multiline"
+      <Box
+      component="form"
+      sx={{
+        '& .MuiTextField-root': { m: 1, width: '60ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+       <TextField
+          id="filled-multiline-static"
+          label="Mission Statement"
+          color="secondary"
           multiline
           rows={4}
-          defaultValue="Default Value"
-          variant="standard"
         />
+      </Box>
       </div>
-    </Box>
+      <div>
+        <Typography variant="h6">
+          <b>Values</b>
+        </Typography>
+      </div>
+      <div class="badges">
+        <br></br>
+      <CardMedia
+      component="img"
+      sx={{
+        height: 100, objectFit: "contain"}}
+        image={require('./temp_imgs/badges.png')}
+    >
+      </CardMedia>
+      <br></br>
+      <Box
+      component="form"
+      sx={{
+        '& .MuiTextField-root': { m: 1, width: '60ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+       <TextField
+          id="filled-multiline-static"
+          color="secondary"
+          label="How does your company build an environment that fosters diversity, equity, and inclusion?"
+          multiline
+          rows={4}
+        />
+      </Box>
+      </div>
+      <div>
+        <Typography variant="h6">
+          <b>Current Openings</b>
+        </Typography>
+
+          <FormControlLabel control={<Switch defaultChecked color="secondary" />} label="Would you like to advertise current open positions through IPP?" />
+
+      </div>
+
+      <div>
+      <br></br>
+        <Typography variant="h6">
+          <b>Future Hiring Timeline</b>
+        </Typography>
+        
+        <Typography variant="body2">
+    Optionally include more information here so we can keep an eye out for future roles.
+    </Typography>
+    <Box
+      component="form"
+      sx={{
+        '& .MuiTextField-root': { m: 1, width: '60ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+       <TextField
+          id="filled-multiline-static"
+          label=""
+          color="secondary"
+          multiline
+          rows={4}
+        />
+      </Box>
+      <br></br>
+      <Button variant="contained" color="secondary"><b>Submit</b></Button>
+      <br></br>
+      <br></br>
+      </div>
+
     </div>
-    </div>
+  </div>
   )
 }
