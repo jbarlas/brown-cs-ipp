23c0f93f85d6f8764a0e87c0ac17ceb322daf3e7
sdacruz1 <stephanie_da_cruz@brown.edu>
2023-01-22 00:00:38 -0500

    job card updates

--- src/components/partner-company-card/CompanyCardLarge.jsx
+++ src/components/partner-company-card/CompanyCardLarge.jsx
@@ -6,19 +6,54 @@ import hiring_icon from '../../assets/partner_icons/hiring_icon.png';
 import profit_icon from '../../assets/partner_icons/class_icon.png';
 import industry_icon from '../../assets/partner_icons/class_icon.png';
 
+import JobCard from "./JobCard.jsx";
+
 export default function CompanyCard() {
   return (
     <div class="partner_company_card_large">
-      {/* Company Logo */}
-      <img class="partner_logo" src={company_logo} alt="company logo"/>
+      <div style={{display: "flex", justifyContent:'space-around'}}>
+        {/* Company Logo */}
+        <img class="partner_logo_large" src={company_logo} alt="company logo"/>
 
-      {/* Info */}
-      <div>
-        <div style={{display: 'flex', alignItems: 'center'}}>
-          <p class="partner_mini_card_title">Company Name</p>  <img class="partner_hiring_icon" src={hiring_icon} alt="hiring icon"/>
+        {/* Info */}
+        <div>
+          {/* Name */}
+          <div style={{display: 'flex', alignItems: 'center'}}>
+            <p class="partner_mini_card_title">Company Name</p>  <img class="partner_hiring_icon" src={hiring_icon} alt="hiring icon"/>
+          </div>
+          <div class="partner_info_text_div">
+            {/* Tags */}
+            <div class="partner_tag_div">
+              <img class="partner_hiring_icon" src={hiring_icon} alt="location icon"/>
+              <p class="partner_mini_card_subtitle">Location</p>
+              <img class="partner_hiring_icon" src={hiring_icon} alt="location icon"/>
+              <p class="partner_mini_card_subtitle">Size</p>
+            </div>
+            <p style= {{width: '20rem'}}>Longer blurb talking about the company that they submit to us for use on this company page.
+            Lorem ipsum dolor sit amet, consectet adipiscing elit.</p>
+          </div>
         </div>
-        <p class="partner_mini_card_subtitle">Location - Size</p>
-        <p>One liner about the company</p>
+      </div>
+
+      {/* Values */}
+      <div>
+        <div style={{display: 'flex', justifyContent: 'space-around', alignItems: 'center'}}>
+          <button>Arrow Left</button>
+          <div style={{display:'flex', alignItems: 'center'}}>
+            <img class="partner_hiring_icon" src={hiring_icon} alt="value badge"/>
+            <p>Value Title</p>
+          </div>
+          <button>Arrow Right</button>
+        </div>
+        <p>Value Text Blurb</p>
+      </div>
+
+      {/* Hiring */}
+      <h2>Hiring</h2>
+      <div style={{display:'flex', flexDirection: 'column', justifyContent: 'space-around'}}>
+        <JobCard/>
+        <JobCard/>
+        <JobCard/>
       </div>
     </div>
   );
---
+++ src/components/partner-company-card/JobCard.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./JobCard.css";
+
+import hiring_icon from '../../assets/partner_icons/hiring_icon.png';
+
+export default function JobCard() {
+  return (
+    <div class="partner_jobcard">
+      <div class="partner_jobcard_header">
+        {/* Job or Internship Icon */}
+        <img class="partner_jobcard_icon" src={hiring_icon} alt="Job or Internship Icon"/>
+        <div style= {{paddingLeft: '1rem'}}>
+          <p class="parter_jobcard_title">Title</p>
+          <p class="parter_jobcard_subtitle">Subtitle</p>
+        </div>
+      </div>
+      <p class="partner_jobcard_blurb"> A small blurb about the job or internship detailing
+      anything the company.  A small blurb about the job or internship detailing anything
+      the company wanted to include. wanted to include. </p>
+      <div style = {{display: 'flex', justifyContent: 'space-between'}}>
+        <p class="partner_jobcard_payrange">Pay Range</p>
+        <button class="partner_jobcard_button">Engage</button>
+      </div>
+    </div>
+  );
+}
