700d3e793142003ca80d3d15748aa7b0aa6410e8
sdacruz1 <stephanie_da_cruz@brown.edu>
2023-03-03 00:53:20 -0500

    undid destructive changes

--- src/components/nav-bar/NavBar.jsx
+++ src/components/nav-bar/NavBar.jsx
@@ -4,16 +4,15 @@ import { Link } from "react-router-dom";
 
 export default function NavBar() {
   return (
-    <div></div>
-    // <div className="navbar-container">
-    //   <div className="navbar-logo">
-    //     <Link to="/">Brown CS IPP</Link>
-    //   </div>
-    //   <div className="navbar-links-container">
-    //     <Link to="/events">Events</Link>
-    //     <Link to="/partners">Partners</Link>
-    //     <Link to="/apply">Application</Link>
-    //   </div>
-    // </div>
+    <div className="navbar-container">
+      <div className="navbar-logo">
+        <Link to="/">Brown CS IPP</Link>
+      </div>
+      <div className="navbar-links-container">
+         <Link to="/events">Events</Link>
+         <Link to="/partners">Partners</Link>
+         <Link to="/apply">Application</Link>
+       </div>
+    </div>
   );
 }
