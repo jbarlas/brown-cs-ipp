23c8e4cfc2316b9bfd1a0f5e25cb3d7e54f525e0
sdacruz1 <stephanie_da_cruz@brown.edu>
2023-03-03 00:49:49 -0500

    destructive big fix to nav bar

--- src/components/nav-bar/NavBar.jsx
+++ src/components/nav-bar/NavBar.jsx
@@ -4,15 +4,16 @@ import { Link } from "react-router-dom";
 
 export default function NavBar() {
   return (
-    <div className="navbar-container">
-      <div className="navbar-logo">
-        <Link to="/">Brown CS IPP</Link>
-      </div>
-      <div className="navbar-links-container">
-        <Link to="/events">Events</Link>
-        <Link to="/partners">Partners</Link>
-        <Link to="/apply">Application</Link>
-      </div>
-    </div>
+    <div></div>
+    // <div className="navbar-container">
+    //   <div className="navbar-logo">
+    //     <Link to="/">Brown CS IPP</Link>
+    //   </div>
+    //   <div className="navbar-links-container">
+    //     <Link to="/events">Events</Link>
+    //     <Link to="/partners">Partners</Link>
+    //     <Link to="/apply">Application</Link>
+    //   </div>
+    // </div>
   );
 }
